b3f056b22845e43359763fddd95b20e0cec121d5
ByoengHyeonKim <120387354+ByoengHyeonKim@users.noreply.github.com>
2022-12-12 23:23:09 +0900

    Update API.py

--- raspberrypi/module/API.py
+++ raspberrypi/module/API.py
@@ -1,10 +1,10 @@
 import os       # 시스템 모듈
 
 # 환경 변수명 지정
-ENV = 'HANGO_URL'
+ENV = 'URL'
 try:
   # URL 환경 변수 불러오기
-  URL = str(os.environ['HANGO_URL'])
+  URL = str(os.environ['URL'])
 except:
   print(f"'{ENV}'는 존재하지 않는 환경 변수입니다.\n먼저, 환경 변수를 등록해주십시오.")
   exit(1)
